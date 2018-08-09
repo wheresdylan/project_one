e334726000ecc5530960857426c7d255c5f6c541
must-git-good <39744893+must-git-good@users.noreply.github.com>
2018-08-09 14:01:16 -0500

    Basic firebase database set up

---
+++ app.js
@@ -0,0 +1,47 @@
+
+//Initialize Firebase:
+
+  var config = {
+    apiKey: "AIzaSyDbC3qvRjfbGRJhzUKcHHHG2xaFemji7qI",
+    authDomain: "project-one-our-food-decider.firebaseapp.com",
+    databaseURL: "https://project-one-our-food-decider.firebaseio.com",
+    projectId: "project-one-our-food-decider",
+    storageBucket: "project-one-our-food-decider.appspot.com",
+    messagingSenderId: "666666151675"
+  };
+  firebase.initializeApp(config);
+
+
+
+//Setting the reference for the firebase database:
+var db = firebase.database();
+
+
+
+//Setting variables for the user's choice inputs:
+    // Below is active code other is for testing
+        // var userOneChoiceOne = $("#user1-choice1");
+        // var userOneChoiceTwo = $("#user1-choice2");
+        // var userTwoChoiceOne = $("#user2-choice1");
+        // var userTwoChoiceTwo = $("#user2-choice2");
+
+var userOneChoiceOne = "BBQ";
+var userOneChoiceTwo = "chinese";
+var userTwoChoiceOne = "indian";
+var userTwoChoiceTwo = "vegetarian";
+
+
+//Document ready check
+$(document).ready();
+
+
+//Pushing those values into the firebase database (for now).
+$(document).on("click", "#submit", function(){
+    console.log("Click worked, should push user values to the db)")
+    db.ref().push({
+        u1c1: userOneChoiceOne,
+        u1c2: userOneChoiceTwo,
+        u2c1: userTwoChoiceOne,
+        u2c2: userTwoChoiceTwo,
+    });
+});
