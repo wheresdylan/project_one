8c266b21a6a89e37d6651fbd55185bbda5b18a79
mv04 <mtvickery1@gmail.com>
2018-08-13 18:58:58 -0500

    added animation to welcome page

--- app.js
+++ app.js
@@ -197,6 +197,15 @@ $(document).ready(function () {
     });
 });
 
+//WELCOME PAGE - GRAY BOX SLIDE OUT ANIMATION//
+$(document).ready(function() {
+    $("#welcome-left").animate({left: "0"}, {
+        duration: 2000    
+    });
+    $("#welcome-right").animate({right: "0"}, {
+        duration: 2000       
+    });
+});
 
 //this is where the map is displayed and populated
 
